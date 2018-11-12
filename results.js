1d68fa0a0c85e17d8a4eedf0f7ce6fec0b18eda8
tempteamcode <38981204+tempteamcode@users.noreply.github.com>
2018-11-12 11:52:09 +0100

    search page

--- results.js
+++ results.js
@@ -1,11 +1,18 @@
 
-function addresult(name, year, picture) {
+function addresult(resname, name, year, picture) {
     results = document.getElementById("results_div");
     result = results.firstElementChild.cloneNode(true);
+    
+    result.style = "cursor: pointer";
+    result.onclick = function() {
+        window.location = "details.html?resource=" + encodeURIComponent(resname);
+    };
     
     result.getElementsByTagName("h2")[0].innerText = name;
     result.getElementsByTagName("h3")[0].innerText = year;
     result.getElementsByTagName("img")[0].src = picture;
     
     results.appendChild(result);
+    return result;
 }
+
