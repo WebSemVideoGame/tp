22914ef85c71d0e2e71fd9dec83d91f6a713d196
Gillpoitou <39384177+Gillpoitou@users.noreply.github.com>
2018-11-14 09:55:11 +0100

    Update index
    - Adding clickable boxes that redirect to result page

--- scripts/index.js
+++ scripts/index.js
@@ -1,12 +1,12 @@
 ﻿
 function init() {
     dbpediaQueryAsync(
-        'select distinct ?name ?genre ?dev where { ?r a dbo:VideoGame. ?r dbp:released ?x. filter(regex(?x,"2018")). ?r foaf:name ?name. ?r dbo:genre ?g. ?g rdfs:label ?genre. ?r dbo:developer ?d. ?d rdfs:label ?dev } order by (?x) LIMIT 1',
+        'select distinct ?r ?name ?genre ?dev where { ?r a dbo:VideoGame. ?r dbp:released ?x. filter(regex(?x,"2018")). ?r foaf:name ?name. ?r dbo:genre ?g. ?g rdfs:label ?genre. ?r dbo:developer ?d. ?d rdfs:label ?dev } order by (?x) LIMIT 1',
         lastGamePublishedInfos
     );
 
     dbpediaQueryAsync(
-        'select distinct ?name ?genre ?dev where {?r a dbo:VideoGame. ?r foaf:name ?name. ?r dbo:genre ?g. ?g rdfs:label ?genre. ?r dbo:developer ?d. ?d rdfs:label ?dev } ORDER BY RAND() LIMIT 10000',
+        'select distinct ?r ?name ?genre ?dev where {?r a dbo:VideoGame. ?r foaf:name ?name. ?r dbo:genre ?g. ?g rdfs:label ?genre. ?r dbo:developer ?d. ?d rdfs:label ?dev } ORDER BY RAND() LIMIT 10000',
         randomGameInfo
     );
 }
@@ -21,6 +21,11 @@ function lastGamePublishedInfos(data) {
     document.getElementById("titleLastGamePublished").innerText = result.name.value;
     document.getElementById("genreLastGamePublished").innerText = result.genre.value;
     document.getElementById("devLastGamePublished").innerText = result.dev.value;
+
+    resource = getRessourceName(result.r.value,result.name.value);
+    document.getElementById("box1").onclick = function(){
+        window.location.href = "details.html?resource=" + encodeURIComponent(resource);
+    }
 }
 
 function randomGameInfo(data){
@@ -34,4 +39,20 @@ function randomGameInfo(data){
     document.getElementById("titleRandomGame").innerText = result.name.value;
     document.getElementById("genreRandomGame").innerText = result.genre.value;
     document.getElementById("devRandomGame").innerText = result.dev.value;
+
+    resource2 = getRessourceName(result.r.value,result.name.value);
+    document.getElementById("box2").onclick = function(){
+        window.location.href = "details.html?resource=" + encodeURIComponent(resource2);
+    }
+}
+
+function getRessourceName(resource,name){
+    var resname;
+				var debuturl = "dbpedia.org/resource/";
+				if (resource.includes(debuturl)) {
+					resname = resource.substring(resource.lastIndexOf(debuturl) + debuturl.length);
+				} else {
+					resname = name;
+                }
+    return resname;
 }
