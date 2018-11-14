cb9a3673c0235804072afd49d906753a06d7260c
Gillpoitou <39384177+Gillpoitou@users.noreply.github.com>
2018-11-14 09:30:16 +0100

    Update on index.html
    -adding second info box

--- scripts/index.js
+++ scripts/index.js
@@ -4,6 +4,11 @@ function init() {
         'select distinct ?name ?genre ?dev where { ?r a dbo:VideoGame. ?r dbp:released ?x. filter(regex(?x,"2018")). ?r foaf:name ?name. ?r dbo:genre ?g. ?g rdfs:label ?genre. ?r dbo:developer ?d. ?d rdfs:label ?dev } order by (?x) LIMIT 1',
         lastGamePublishedInfos
     );
+
+    dbpediaQueryAsync(
+        'select distinct ?name ?genre ?dev where {?r a dbo:VideoGame. ?r foaf:name ?name. ?r dbo:genre ?g. ?g rdfs:label ?genre. ?r dbo:developer ?d. ?d rdfs:label ?dev } ORDER BY RAND() LIMIT 10000',
+        randomGameInfo
+    );
 }
 
 function lastGamePublishedInfos(data) {
@@ -16,4 +21,17 @@ function lastGamePublishedInfos(data) {
     document.getElementById("titleLastGamePublished").innerText = result.name.value;
     document.getElementById("genreLastGamePublished").innerText = result.genre.value;
     document.getElementById("devLastGamePublished").innerText = result.dev.value;
+}
+
+function randomGameInfo(data){
+    var rand = Math.floor(Math.random() * Object.keys(data.results.bindings).length);
+    var result = data.results.bindings[rand];
+
+    imageWp(result.name.value, function(url) {
+        document.getElementById("imageRandomGame").src = url;
+    });
+    
+    document.getElementById("titleRandomGame").innerText = result.name.value;
+    document.getElementById("genreRandomGame").innerText = result.genre.value;
+    document.getElementById("devRandomGame").innerText = result.dev.value;
 }
